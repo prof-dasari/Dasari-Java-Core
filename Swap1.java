b846aa08c96dc0870b6c39e8aef241039eca7aba
Dasari <sree91@gmail.com>
2024-07-20 12:15:17 +0530

    Update Swap1.java

--- Swap1.java
+++ Swap1.java
@@ -10,7 +10,7 @@ class Demo {
     }
 }
 
-class Test {
+class Swap1 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         System.out.println("Enter a value");
